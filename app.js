74b04b563346e808fcbf5149120e36a4a7819b52
FurkanBey123 <furkanboran123@gmail.com>
2022-12-07 00:00:07 +0000

    Security update

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
-const app = express();
+require('dotenv').config()
+require('express-async-errors')
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+const express = require('express')
+const app = express()
 
 // connect db
-const connectDB = require('./db/connect');
+const connectDB = require('./db/connect')
 
 // authorization
 const authenticateUser = require('./middleware/authentication')
@@ -14,18 +21,27 @@ const authRouter = require("./routes/auth")
 const itemsRouter = require("./routes/items")
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
-app.use(express.json());
 // extra packages
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 300 // limit each IP to 100 requests per windowMs
+}))
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/items", authenticateUser, itemsRouter)
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+// Middlewares
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 3000;
 
@@ -36,8 +52,8 @@ const start = async () => {
       console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
-start();
+start()
--- controllers/items.js
+++ controllers/items.js
@@ -1,36 +1,41 @@
 const Item = require('../models/Item')
-const {StatusCodes} = require('http-status-codes')
-const {BadRequestError, NotFoundError} = require('../errors')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, NotFoundError } = require('../errors')
 
 const getAllItems = async (req, res) => {
     const items = await Item.find().sort("name")
-    res.status(StatusCodes.OK).json({items, count: items.length})
+    res.status(StatusCodes.OK).json({ items, count: items.length })
 }
 
 const getItem = async (req, res) => {
-    const item = await Item.find({name: req.params.name})
+    const item = await Item.findOne({ name: req.params.name })
     if (!item) {
         throw new NotFoundError("No item found with name '" + req.params.name + "'")
     }
-    res.status(StatusCodes.OK).json({item, count: item.length})
+    res.status(StatusCodes.OK).json({ item, count: item.length })
 }
 
 const createItem = async (req, res) => {
     const item = await Item.create({ ...req.body })
-    res.status(StatusCodes.CREATED).json({item})
+    res.status(StatusCodes.CREATED).json({ item })
 }
 
 const updateItem = async (req, res) => {
     if (req.params.name === '') {
         throw new BadRequestError("Item name can not be empty!")
     }
-    const item = await Item.findOneAndUpdate({name: req.params.name}, req.body, {new: true,
-        upsert: true})
-    res.status(StatusCodes.OK).json({item})
+    const item = await Item.findOneAndUpdate({ name: req.params.name }, req.body, {
+        new: true,
+        upsert: true
+    })
+    if (!item) {
+        throw new NotFoundError("No item found with name '" + req.params.name + "'")
+    }
+    res.status(StatusCodes.OK).json({ item })
 }
 
 const deleteItem = async (req, res) => {
-    const item = await Item.deleteMany({name: req.params.name})
+    const item = await Item.deleteMany({ name: req.params.name })
     if (!item) {
         throw new NotFoundError("No item found with name '" + req.params.name + "'")
     }
